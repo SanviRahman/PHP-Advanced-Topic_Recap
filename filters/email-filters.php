4d66355476c54e9e5e1759e1d8992700f74403bf
Sheikh Forid Ahmed Shanto <Sheikh15-3700@diu.edu.bd>
2025-09-22 17:19:54 +0600

    Done

---
+++ filters/email-filters.php
@@ -0,0 +1,13 @@
+<?php
+    $email = "shanto@gmail.com";
+
+    //Remove all illegal characters from email
+    $email = filter_var($email,FILTER_SANITIZE_EMAIL);
+
+    //validate email
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL) === false){
+        echo "$email is a valid email address";
+    }else{
+        echo  "$email is not a valid email address";
+    }
+?>
